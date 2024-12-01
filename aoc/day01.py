79ee0319e69e9c107256ea48050661524fed6a7a
michael-foster <mike@tonebaby.com>
2024-12-01 09:33:47 -0800

    feat(day 1): :sparkles: Complete day 1 challenge

--- aoc/day01.py
+++ aoc/day01.py
@@ -1,5 +1,6 @@
 """01: PROBLEM NAME"""
 import aoc.util
+from collections import Counter
 
 
 # all solutions should subclass the `Solver` exposed by `aoc.util`
@@ -11,11 +12,20 @@ class Solver(aoc.util.Solver):
 
         # optionally do something with self.input, like parsing it to a more
         # useful representation and storing it in the instance
+        self.c1, self.c2 = map(list, zip(*[map(int, line.split()) for line in input.splitlines()]))
+        self.c1.sort()
+        self.c2.sort()
 
     def part_one(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        sum = 0
+        for i in range(len(self.c1)):
+            sum += abs(self.c1[i] - self.c2[i])
+        return sum
 
     def part_two(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        sum = 0
+        frequency = Counter(self.c2)
+        for i in range(len(self.c1)):
+            n = self.c1[i]
+            sum += n * frequency.get(n, 0)
+        return sum
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -7,10 +7,10 @@ from aoc.util import Solution
 #############################
 # ======= solutons =========#
 #############################
-EXAMPLE_PART_ONE = 0
-EXAMPLE_PART_TWO = 0
-PART_ONE = 0
-PART_TWO = 0
+EXAMPLE_PART_ONE = 11
+EXAMPLE_PART_TWO = 31
+PART_ONE = 2264607
+PART_TWO = 19457120
 
 
 #############################
